d8b5beff10353831de6a13238762a6d82da8661b
2G-Afroz <siddiquiafroz15@gmail.com>
2024-03-25 17:36:20 +0530

    Fixed ERROR occuring dur to importing page with different name.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
-import Signin from './pages/SignIn'
+import SignIn from './pages/SignIn'
 
 
 import React from 'react'
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>      
   )
