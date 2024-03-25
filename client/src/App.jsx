bac489ea0dcfff10d4fdfe492942f60a51f7a23d
2G-Afroz <siddiquiafroz15@gmail.com>
2024-03-25 18:08:03 +0530

    Completed Heading Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,14 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
-
+import Header from './components/Header'
 
 import React from 'react'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
