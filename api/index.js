de479e5945a4c38414faedf9d91dc0feb98fa4db
2G-Afroz <siddiquiafroz15@gmail.com>
2024-03-27 16:05:48 +0530

    Created a middleware and a function to handle error.

--- api/index.js
+++ api/index.js
@@ -21,3 +21,16 @@ app.listen(3000, () => {
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
+
+
+// This middleware is used to generate error.
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,7 +4,7 @@ import bcryptjs from "bcryptjs";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", async (req, res, next) => {
 	const {username, email, password} = req.body;
 	const hashedPass = bcryptjs.hashSync(password, 10);
 	const user = new User({username, email, password: hashedPass});
@@ -13,7 +13,7 @@ router.post("/signup", async (req, res) => {
 		res.status(201).json("User created successfully" + user);
 	}
 	catch(err) {
-		res.status(500).json(err.message);
+		next(err);
 	}
 });
 
