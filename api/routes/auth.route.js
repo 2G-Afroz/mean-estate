ed7c0c7447947d2eeb910b61e1ea03b48eb8cc9c
2G-Afroz <siddiquiafroz15@gmail.com>
2024-03-28 17:51:42 +0530

    Created Sign in route.

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -11,6 +13,27 @@ router.post("/signup", async (req, res, next) => {
 	try {
 		await user.save();
 		res.status(201).json("User created successfully");
+	}
+	catch(err) {
+		next(err);
+	}
+});
+
+router.post("/signin", async (req, res, next) => {
+	const { email, password } = req.body;
+
+	try {
+		const validUser = await User.findOne({email});
+		if(!validUser) return next(errorHandler(404, "User not found."));
+		const compPass = bcryptjs.compareSync(password, validUser.password);
+		if(!compPass) return next(errorHandler(401, "Wrond Email or Password."));
+		const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+		const { password: pass, ...user } = validUser._doc;
+		res
+		.cookie("access_token", token, {httpOnly: true})
+		.status(200)
+		.json(user);
+
 	}
 	catch(err) {
 		next(err);
