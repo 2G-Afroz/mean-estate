e583814694ff9c1d7abbeace936cf00dd31ee297
2G-Afroz <siddiquiafroz15@gmail.com>
2024-04-10 08:35:26 +0530

    Updated the Header and made profile page private.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import Header from './components/Header'
 
 import React from 'react'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -16,9 +17,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>      
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,45 @@
-import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+	const { currentUser } = useSelector((state) => state.user);
+	console.log(currentUser);
   return (
-	<header className='bg-slate-200 shadow-md'>
-		<div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-		<Link to={'/'}>
-		<h1 className='font-bold text-sm sm:text-xl flex-wrap'>
-			<span className='text-slate-500'>Afroz</span>
-			<span className='text-slate-700'>Estate</span>
-		</h1>
-		</Link>
-		<form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-			<input className="bg-transparent focus:outline-none w-24 sm:w-64" type="text" placeholder='Search...'/>
-			<FaSearch className='text-slate-600'/>
-		</form>
-		<ul className='flex gap-4'>
-			<Link to={"/"}>
-			<li className='text-slate-700 hover:underline'>Home</li>
-			</Link>
-			<Link to={"/about"}>
-			<li className='text-slate-700 hover:underline'>About</li>
-			</Link>
-			<Link to={"/signin"}>
-			<li className='text-slate-700 hover:underline'>Sign In</li>
-			</Link>
-		</ul>
-		</div>
-	</header>
-  )
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex-wrap">
+            <span className="text-slate-500">Afroz</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            type="text"
+            placeholder="Search..."
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className="text-slate-700 hover:underline">Home</li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="text-slate-700 hover:underline">About</li>
+          </Link>
+
+
+          <Link to={"/profile"}>
+					{ currentUser ? (
+						<img src={currentUser.avatar} alt={currentUser.name} className="w-7 h-7 rounded-full" />
+						) : (
+            	<li className="text-slate-700 hover:underline">Sign In</li>
+					)}
+					</Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
