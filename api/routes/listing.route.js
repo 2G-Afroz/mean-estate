fc0415ee396e2428c92a05b9446f9db72b8a8e87
2G-Afroz <siddiquiafroz15@gmail.com>
2024-04-12 18:46:35 +0530

    Created Listing model and API route.

---
+++ api/routes/listing.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import Listing from '../models/listing.model.js';
+
+const router = express.Router();
+
+router.post("/create", verifyToken, async (req, res, next) => {
+	try {
+		const listing = await Listing.create(req.body);
+		return res.status(201).json(listing);
+	}
+	catch(err) {
+		next(err);
+	}
+});
+
+export default router;
