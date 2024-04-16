f6b1771967251cf565c60c51171c15ee596ebce4
2G-Afroz <siddiquiafroz15@gmail.com>
2024-04-16 17:02:50 +0530

    Completed About Page.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function About() {
   return (
 	<div>
-		About
+		<h1 className='text-3xl font-bold text-slate-700'>About Afroz Estate</h1>
+		<p>
+			Afroz Estate is a real estate company that provides a platform for people to buy, sell, or rent properties. We have a wide range of properties to choose from.
+
+		</p>
 	</div>
   )
 }
